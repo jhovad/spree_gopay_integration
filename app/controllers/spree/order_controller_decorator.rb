c43dc077586e3770a9fa607780a1ad2bf31580d4
Josef Hovad <josef.hovad@nobord.com>
2017-07-18 12:22:50 +0200

    move action for renew payment from Spree::OrderController to Spree::PaygoController and added cancan/authorize check

--- app/controllers/spree/order_controller_decorator.rb
+++ app/controllers/spree/order_controller_decorator.rb
@@ -10,38 +10,4 @@ Spree::OrdersController.class_eval do
     end
   end
   
-  # in case the transaction failed, user can establish the new one
-  def new_gopay_transaction
-    puts "a"
-    order = Spree::Order.find_by!(number: params[:id])
-    puts "b"
-    payment = order.payments.last
-    puts "c"
-      
-    # check the payment failed
-    if payment.state == "failed" && payment.payment_method.kind_of?(Spree::PaymentMethod::Gopay)     
-      puts "d"
-      gopay_payment = GopayHelper.prepare_payment(order, request)
-      
-      # establish the gopay payment
-      establish_response = GoPay::Payment.create(gopay_payment)
-  		
-      # store important data as *source* of the Payment
-      transaction = GopayHelper.create_transaction(establish_response)
-      
-      # create payment with right relation to source
-      payment_method = Spree::PaymentMethod.find_by_type("Spree::PaymentMethod::Gopay")
-      order.payments.create(amount: order.total, payment_method: payment_method, source: transaction)
-      redirect_to order_path(order) and return
-    end
-    
-  rescue StandardError => e
-    gopay_error(e)
-  end
-
-  def gopay_error(e = nil)
-    @order.errors[:base] << "GoPay error #{e.try(:message)}"
-    render :edit
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.draw do
   get '/gopay/notify', to: 'gopay#notify'
-  get '/order/new_gopay_transaction/:id', to: 'orders#new_gopay_transaction', as: :new_gopay_transaction
+  get '/order/new_gopay_transaction/:id', to: 'gopay#new_gopay_transaction', as: :new_gopay_transaction
 end
